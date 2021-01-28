ad96e7df7bc838b6821462185b0f954cdeb63c0f
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-01-28 12:38:37 +0900

    delete firebase/config.ts

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDh3LS8baTtUyi2wTz133TSOGrXDwCdoWo",
-    authDomain: "fr-sample-ts2.firebaseapp.com",
-    projectId: "fr-sample-ts2",
-    storageBucket: "fr-sample-ts2.appspot.com",
-    messagingSenderId: "275623300977",
-    appId: "1:275623300977:web:94db7aa8bc0531fe26f0d0",
-    measurementId: "G-QXD3SC8MZW"
-}
