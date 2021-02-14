3929d5472a342d785c5ad4179f220533a33dc389
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-02-14 16:15:33 +0900

    remove api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,4 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDh3LS8baTtUyi2wTz133TSOGrXDwCdoWo",
-    authDomain: "fr-sample-ts2.firebaseapp.com",
-    projectId: "fr-sample-ts2",
-    storageBucket: "fr-sample-ts2.appspot.com",
-    messagingSenderId: "275623300977",
-    appId: "1:275623300977:web:94db7aa8bc0531fe26f0d0",
-    measurementId: "G-QXD3SC8MZW"
-}
-
-/*
-export const firebaseConfig = {
     apiKey: "",
     authDomain: "",
     projectId: "",
@@ -18,4 +7,3 @@ export const firebaseConfig = {
     appId: "",
     measurementId: ""
 }
-*/
