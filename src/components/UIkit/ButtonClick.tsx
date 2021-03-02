25bd0216c0f158000a61b900bdcc709dad87d94e
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-03-02 21:29:54 +0900

    add color property

--- src/components/UIkit/ButtonClick.tsx
+++ src/components/UIkit/ButtonClick.tsx
@@ -3,22 +3,23 @@ import { makeStyles, Button } from '@material-ui/core'
 
 type Props = {
     startIcon: string | JSX.Element
+    color: "inherit" | "default" | "primary" | "secondary" | undefined
     label: string
     onClick: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void)
 }
 
 const useStyles = makeStyles({
     root: {
-        margin: '25px auto'
+        margin: '25px'
     }
 })
 
-const ButtonPrimary: FC<Props> = ({startIcon, label, onClick}) => {
+const ButtonClick: FC<Props> = ({startIcon, color, label, onClick}) => {
     const classes = useStyles()
     return (
         <Button
             className={classes.root}
-            color="primary"
+            color={color}
             startIcon={startIcon}
             variant="contained"
             onClick={onClick}
@@ -28,4 +29,4 @@ const ButtonPrimary: FC<Props> = ({startIcon, label, onClick}) => {
     )
 }
 
-export default ButtonPrimary
+export default ButtonClick
