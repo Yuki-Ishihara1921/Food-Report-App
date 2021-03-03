8a802bb440363234d0aef1dc38a30249e8361836
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-03-03 19:01:54 +0900

    change fileName sort

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,3 +1,3 @@
-export { default as Header } from './Header'
 export { default as AppLogo } from './AppLogo'
+export { default as Header } from './Header'
 export { default as SideBarDrawer } from './SideBarDrawer'
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as SignIn } from './SignIn'
 export { default as SignUp } from './SignUp'
-export { default as ReportList } from './ReportList'
 export { default as ReportDetail } from './ReportDetail'
 export { default as ReportEdit } from './ReportEdit'
+export { default as ReportList } from './ReportList'
